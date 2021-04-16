9de47602cb8e254a0757ad254719431a8f430bd2
mickeey2525 <joypeoplemailby@gmail.com>
2021-04-16 10:34:47 +0900

    added ip check code

--- hello.ts
+++ hello.ts
@@ -1,6 +1,12 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+  const json = JSON.stringify({
+    IP: `${ip}`
+  })
+  const response = new Response(json, {
+    headers: { "content-type": "application/json; charset=UTF-8", },
   });
   event.respondWith(response);
 });
