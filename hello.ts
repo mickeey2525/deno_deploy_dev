cacec363699d16b245f055ed511a12243b46e756
mickeey2525 <m.tachibana25@gmail.com>
2022-03-31 15:48:14 +0900

    return request as it is

--- hello.ts
+++ hello.ts
@@ -1,6 +1,5 @@
 addEventListener("fetch", (event) => {
-  const json = JSON.stringify(
-      {body: `${event.request.body}`, headers: `${event.request.headers.values()}` })
+  const json = JSON.stringify(event.request);
   const response = new Response(json, {
     headers: { "content-type": "application/json; charset=UTF-8", },
   });
