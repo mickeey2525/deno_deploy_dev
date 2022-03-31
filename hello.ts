efe69e1cf867617c9c2c91c40a0a1b00d6f9daf5
mickeey2525 <m.tachibana25@gmail.com>
2022-03-31 15:36:17 +0900

    return request as it is

--- hello.ts
+++ hello.ts
@@ -1,11 +1,5 @@
 addEventListener("fetch", (event) => {
-  // Get the value of the header named `x-forwarded-for`.
-  // The value can either be a IPv4 or IPv6 address.
-  const ip = event.request.headers.get("x-forwarded-for");
-  const json = JSON.stringify({
-    IP: `${ip}`
-  })
-  const response = new Response(json, {
+  const response = new Response(event.reqeust, {
     headers: { "content-type": "application/json; charset=UTF-8", },
   });
   event.respondWith(response);
